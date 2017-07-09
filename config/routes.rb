ee54c939fab42f0bec76f2b5ddf610909eb170d6
ericash0923 <eric@gaglani.com>
2017-07-09 22:10:19 +0000

    add toggle status for draft or published---blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   resources :credentials, except: [:show]
   get 'portfolio/:id', to:'credentials#show', as:'portfolio_show'
-  resources :blogs
+  
   
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   
 end
