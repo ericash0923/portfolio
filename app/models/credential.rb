b6a723c982717d24877f3bc21553b09e4ff4aa93
ericash0923 <eric@gaglani.com>
2017-07-11 01:10:01 +0000

    setup image defaults value

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -4,4 +4,15 @@ class Credential < ApplicationRecord
  def self.angular
   where(subtitle: 'Augluar')
  end
+ 
+ scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+ 
+ after_initialize :set_defaults
+ 
+ def set_defaults
+  self.main_image ||= "http://via.placeholder.com/600x400"
+  self.thumb_image ||= "http://via.placeholder.com/350x200"
+ end
 end
+
+
