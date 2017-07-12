26648f5869479ab7105bda6419b759db5fbf5f0e
ericash0923 <eric@gaglani.com>
2017-07-12 20:28:49 +0000

    customized login, logout and signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :credentials, except: [:show]
   get 'angular-items', to:'credentials#angular'
   get 'portfolio/:id', to:'credentials#show', as:'portfolio_show'
