7467480f4f692e4d35d2b868822443084159528f
ericash0923 <eric@gaglani.com>
2017-07-31 23:47:07 +0000

    update credential to include a position attr

--- app/controllers/credentials_controller.rb
+++ app/controllers/credentials_controller.rb
@@ -4,7 +4,7 @@ class CredentialsController < ApplicationController
  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
  def index
-  @portfolio_items = Credential.all
+  @portfolio_items = Credential.by_position
  end
  
  def angular
