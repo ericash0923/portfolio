423ab9c7ba266c486285b37996bbe81b2c69b106
ericash0923 <eric@gaglani.com>
2017-08-12 23:01:36 +0000

    update production.rb calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_cable.allowed_request_origins = ['https://ea-devcamp-portolio.herokuapp.com', 'http://ea-devcamp-portolio.herokuapp.com']
+  config.action_cable.url = "wss://ea-devcamp-portolio.herokuapp.com/cable"
 end
