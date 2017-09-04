9a6d949083d164e45a449e74f0a1590dee5c6b62
ericash0923 <eric@gaglani.com>
2017-09-04 23:34:03 +0000

    implemented new, edit and destroy for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,6 @@
 class TopicsController < ApplicationController
   before_action :set_sidebar_topics
+  before_action :set_topic_item, only: [:edit, :show, :update]
   layout 'topic'
 
   def index
@@ -16,8 +17,56 @@ class TopicsController < ApplicationController
     end
   end
   
-  private 
+  def new
+    @topic = Topic.new
+  end
   
+  def create
+  @topic = Topic.new(topic_params)
+
+    respond_to do |format|
+      if @topic.save
+        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    
+    respond_to do |format|
+      if @topic.update(topic_params)
+        format.html { redirect_to topic_path, 
+        notice: 'Topic item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+  
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+     respond_to do |format|
+       format.html { redirect_to topics_url, 
+       notice: 'Topic was successfully destroyed.' }
+     end
+  end
+  
+  private
+  
+  def topic_params
+      params.require(:topic).permit(:title)
+  end
+  
+  def set_topic_item
+    @topic = Topic.find(params[:id])
+  end
+   
   def set_sidebar_topics
     @side_bar_topics = Topic.with_blogs
   end
