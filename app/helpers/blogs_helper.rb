c6f4e78602ab90eb2454769431824c86f0ede622
ericash0923 <eric@gaglani.com>
2018-01-12 08:09:31 +0000

    fix bug

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,4 +1,8 @@
 module BlogsHelper
+
+  require 'redcarpet'
+  require 'coderay'
+  
  def gravtar_helper user
   image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
  end
